2a19b3c684b4a9b2fd1502f3b502ec46feb25fec
Rowan Jansens <rowan.jansens@gmail.com>
2020-07-21 18:19:30 -0600

    made some changes to the code to allow for better graphing

--- attenuation.c
+++ attenuation.c
@@ -6,8 +6,7 @@ Coded by: Rowan Jansens
 #include <stdlib.h>
 #include <math.h>
 
-double interval;
-int photons[125][4];
+int photons[125][6];
 
 //Data table with values for calculating atmopsheric density
 //https://en.wikipedia.org/wiki/Barometric_formula
@@ -23,12 +22,18 @@ double atmosphere[8][4] = {
   {100000, 0, 1, -1}  //0 density layer makes computation faster
 };
 
-double density(double height){
-  //determine atmosphere layer (0-6) to sorce atmospheric data
+int get_layer(double height){
+  //determine atmosphere layer (0-7) to sorce atmospheric data
   int layer = 0;
   while (height >= atmosphere[layer+1][0] && layer < 7){
     layer++;
   }
+  return layer;
+}
+  
+double density(double height){
+  int layer = get_layer(height);
+ 
   //source values from "atmosphere" datatable depending on layer
   double density, l_height, l_den, l_temp, l_lapse, base, exponent;
   l_height = atmosphere[layer][0];
@@ -36,7 +41,10 @@ double density(double height){
   l_temp = atmosphere[layer][2];
   l_lapse = atmosphere[layer][3];
 
-  if (l_lapse != 0){
+  if (layer == 7){
+    return 0;
+  }
+  else if (l_lapse != 0){
     //build the first equation and return result
     base = l_temp / (l_temp + l_lapse * (height - l_height));
     exponent = 1 + ((9.80665 * 0.0289644) / (8.3144598 * l_lapse));
@@ -55,11 +63,11 @@ double density(double height){
 //integrate the density function (^above^) using trapezoidal riemann sums
 //and divide to come up with average density for a particlar interval
 //If the altitude is over 100 km, then return 0.
-double average_density(double height){
+double average_density(double height, double interval){
   double i;
   double area = 0;
   double step = 0.25; //decreasing this number will increase run time and accuracy
-  if(height < 100000){
+  if(height <= 100000){
     for (i=height; i<(height+interval); i+=step){
       area += ((density(i) + density(i + step)) / 2) * step;
     }
@@ -86,49 +94,69 @@ int make_photons(double start_height){
 //equation comes from a gnuplot fit of NIST attenuation data
 //https://physics.nist.gov/PhysRefData/XrayMassCoef/ComTab/air.html
 double get_attcoe(double energy){
-  double attcoe = 393.444 / (pow(energy, 2.87995) + 0.0911984) + 0.0417973;
+  double attcoe = 393.265 / (pow(energy, 2.89148) + 0.0990357) + 0.0449311;
   return attcoe;
 }
 
-//update the photon matrix by replacing the inital photons with the remaing
-//photons after a specific interval by calculating the trasmission percentage
-//from density, attenuation coeffiect, and the size of the interval
-double photons_prime(double height){
-  int i;
-  for (i=0; i<125; i++){  //loop through all energies
-    int e = photons[i][0];
-    double percent = exp(-1 * get_attcoe(e) * average_density(height) * interval);
-    photons[i][1] = height + interval;  //update the "iteration-number" column in the matrix
-    photons[i][2] *= percent;  //update the "photon-number" column in the matrix
-  }
+double percent(double height, double interval, int energy){
+  double percent = exp(-1 * get_attcoe(energy) * average_density(height, interval) * interval);
+  return percent;
 }
 
-int main(){
-  
-  double start_height;   //altitude of detonation
-  interval = 2000;  //how often the matrix will be sampled/updated
-  //changing the interval should have no major effect on the final results
-  //becuase the calculations are always made using a continuous density function
-  //which is averaged out over the interval
+double surface_evolution(double start_height, double interval){
 
-  printf("Enter Detonation Altitude (meters above MSL [0m-100000m]): ");
-  scanf("%lf", &start_height);
-
-
-  FILE * f = fopen("att.dat", "w");
   int h, i, j;
+  FILE * f = fopen("surface_evolution.dat", "w");
   make_photons(start_height);
   
   for(h=start_height; h<=100000; h+=interval){   //increment the altitude
     for (i=0; i<125; i+=5){   //loop through some of the energies
       //increasing the incriment will increase run time
-      fprintf(f, "%d %d %d\n", photons[i][0], photons[i][1], photons[i][2]);  //save matrix to file
+      fprintf(f, "%d %d %d\n", photons[i][1], photons[i][0], photons[i][2]);  //save matrix to file
   }
     fprintf(f, "\n");
-    photons_prime(h);  //update matrix
+    for (i=0; i<125; i++){
+      photons[i][1] = h + interval;  //update the "iteration-number" column in the matrix
+      photons[i][2] *= percent(h, interval, photons[i][0]);  //update the "photon-number" column in the matrix
+    }
+  }
+  fclose(f);
+}
+
+double before_after(double start_height, double end_height){
+  int i;
+  FILE * f = fopen("before_after.dat", "w");
+
+  make_photons(start_height);
+  for (i=0; i<125; i++){
+    photons[i][3] = photons[i][2] * percent(start_height, end_height - start_height, photons[i][0]);
   }
 
+  for (i=0; i<125; i++){
+    fprintf(f, "%d, %d, %d\n", photons[i][0], photons[i][2], photons[i][3]);
+  }
   fclose(f);
+}
+
+
+int main(){
+  int j;
+  int i = 0;
+  
+  double start_height = 40000; //altitude of detonation
+  double end_height = 100000;
+  double interval = 2000;
+  //changing the interval should have no major effect on the final results
+  //becuase the calculations are always made using a continuous density function
+  //which is averaged out over the interval
+
+  //printf("Enter Detonation Altitude (meters above MSL [0m-100000m]): ");
+  // scanf("%lf", &start_height);
+  
+
+  surface_evolution(start_height, interval);
+  //before_after(start_height, end_height);
+
 
   return 0;
 }
